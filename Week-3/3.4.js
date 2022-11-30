497b59fdff35266efff1dd9b3216e347d50b941c
Abhishek Gogoi <44311710+AbhishekGogoi@users.noreply.github.com>
2022-11-30 17:58:35 +0530

    Updated 3.4.js

--- Week-3/3.4.js
+++ Week-3/3.4.js
@@ -1,26 +1,5 @@
-// function createStack(){
-//     return {
-//         items:[],
-//         push(item){
-//             this.items.push(item)
-//         },
-//         pop(){
-//             return this.items.pop();
-//         }
-//     };
-// }
-
-// const stack = createStack()
-// stack.push(10)
-// stack.push(5)
-// stack.pop()
-// stack.items =[10,100,1000]
-// console.log(stack)
-
-// refactored code  
-
 function createStack() {
-    const items = [];
+  const items = [];
     return {
         push(item) {
             items.push(item)
